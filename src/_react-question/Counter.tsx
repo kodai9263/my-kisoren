4d71c5d9d3a43751a668f9edc81eaebc84e46cbf
kodai9263 <kou92638685@gmail.com>
2026-01-14 07:28:05 +0900

    chore: remove commented-out code and excessive whitespace in Counter.tsx

--- src/_react-question/Counter.tsx
+++ src/_react-question/Counter.tsx
@@ -5,3 +5,51 @@
 - リセットボタンで0に戻る
 - TypeScriptで型をつける*/
 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/*import { useState } from "react";
+
+export default function Counter() {
+  const [time, setTime] = useState<number>(0);
+
+  const plusSubmit = () => setTime((prev) => prev + 1);
+  const minusSubmit = () => setTime((prev) => prev - 1);
+  const resetSubmit = () => setTime(0);
+
+  return (
+    <div>
+      <p>{time}</p>
+
+      <button onClick={plusSubmit}>+1</button>
+      <button onClick={minusSubmit}>-1</button>
+      <button onClick={resetSubmit}>リセット</button>
+    </div>
+  );
+}*/
