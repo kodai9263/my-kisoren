8fcb0840e1c41033ef7bb546312578d8ac7a0f94
kodai9263 <kou92638685@gmail.com>
2026-01-13 13:32:26 +0900

    feat: hairetuに答えを追加

--- src/_ts-question/hairetu.ts
+++ src/_ts-question/hairetu.ts
@@ -7,4 +7,43 @@ const numbers = [10, 20, 30, 40, 50];
 //   max: 50,
 //   min: 10
 // }
-console.log(calculateStats(numbers));
+console.log(calculateStates(numbers));
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/*interface State {
+  sum: number;
+  average: number;
+  max: number;
+  min: number;
+}
+
+const calculateStates = (args: number[]): State => {
+  let sum = 0;
+  args.forEach((num) => (sum += num));
+
+  const average = sum / args.length;
+
+  const max = Math.max(...args);
+
+  const min = Math.min(...args);
+
+  return { sum, average, max, min };
+};*/
