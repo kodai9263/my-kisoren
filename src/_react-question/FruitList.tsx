c71438bf984c3eedabf3a17ff7d7bae5acaa2bf0
kodai9263 <kou92638685@gmail.com>
2026-01-21 12:03:08 +0900

    feat: add FruitList component to display a list of fruits

---
+++ src/_react-question/FruitList.tsx
@@ -0,0 +1,54 @@
+// 与えられた配列 const fruits = ['りんご', 'みかん', 'バナナ'] を利用して、それぞれをリスト（<ul>）として画面に表示するコンポーネントを作成してください。
+
+export default function FruitList() {
+  const fruits = ['りんご', 'みかん', 'バナナ'];
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*return (
+  <div>
+    <ul>
+      {fruits.map((fruit, index) => (
+        <li key={index}>{fruit}</li>
+      ))}
+    </ul>
+  </div>
+)*/
